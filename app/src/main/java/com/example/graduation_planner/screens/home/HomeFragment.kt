4b7480ca462d7c0584402974f9a72d42aa2dae4d
aizatazhar <aizat.azharr@gmail.com>
2021-01-02 01:48:04 +0800

    Build skeleton for home fragment

--- app/src/main/java/com/example/graduation_planner/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/graduation_planner/screens/home/HomeFragment.kt
@@ -6,27 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.graduation_planner.R
+import com.example.graduation_planner.models.SampleModules
 
-class Home : Fragment() {
-
-    companion object {
-        fun newInstance() = Home()
-    }
-
+class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root = inflater.inflate(R.layout.home_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        viewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
+        val textView: TextView = root.findViewById(R.id.textView)
+        textView.text = SampleModules.getSampleModules().toString()
 
+        return root
+    }
 }
