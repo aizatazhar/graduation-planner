71d9c4041a724aceff83f903ef85e380edb9d6bb
aizatazhar <aizat.azharr@gmail.com>
2021-01-04 03:49:36 +0800

    Display all the required graduation requirements

--- app/src/main/java/com/example/graduation_planner/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/graduation_planner/screens/home/HomeFragment.kt
@@ -8,17 +8,37 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.graduation_planner.R
+import com.example.graduation_planner.models.GraduationRequirements
 import com.example.graduation_planner.models.SampleModules
 
 class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.home_fragment, container, false)
 
         viewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
-        val textView: TextView = root.findViewById(R.id.textView)
-        textView.text = SampleModules.getSampleModules().toString()
+
+        val tvUlr: TextView = root.findViewById(R.id.tvUlr)
+        tvUlr.text = getString(R.string.tvUlrText, viewModel.satisfiesUlr.toString())
+
+        val tvCsFoundations: TextView = root.findViewById(R.id.tvCsFoundations)
+        tvCsFoundations.text = getString(R.string.tvCsFoundationsText, viewModel.satisfiesCsFoundations.toString())
+
+        val tvCsBreadthAndDepth: TextView = root.findViewById(R.id.tvCsBreadthAndDepth)
+        tvCsBreadthAndDepth.text = getString(R.string.tvCsBreadthAndDepthText, viewModel.satisfiesCsBreadthAndDepth.toString())
+
+        val tvIndustrialExperience: TextView = root.findViewById(R.id.tvIndustrialExperience)
+        tvIndustrialExperience.text = getString(R.string.tvIndustrialExperienceText, viewModel.satisfiesIndustrialExperience.toString())
+
+        val tvItProfessionalism: TextView = root.findViewById(R.id.tvItProfessionalism)
+        tvItProfessionalism.text = getString(R.string.tvItProfessionalismText, viewModel.satisfiesItProfessionalism.toString())
+
+        val tvMathematicsAndSciences: TextView = root.findViewById(R.id.tvMathematicsAndSciences)
+        tvMathematicsAndSciences.text = getString(R.string.tvMathematicsAndSciencesText, viewModel.satisfiesMathematicsAndSciences.toString())
+
+        val tvCredits: TextView = root.findViewById(R.id.tvCredits)
+        tvCredits.text = getString(R.string.tvCreditsText, viewModel.satisfiesCredits.toString())
 
         return root
     }
--- app/src/main/java/com/example/graduation_planner/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/graduation_planner/screens/home/HomeViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.graduation_planner.screens.home
 
 import androidx.lifecycle.ViewModel
+import com.example.graduation_planner.models.GraduationRequirements
+import com.example.graduation_planner.models.Module
+import com.example.graduation_planner.models.SampleModules
 
 class HomeViewModel : ViewModel() {
-
+    private val modules: List<Module> = SampleModules.getSampleModules()
+    val satisfiesUlr: Boolean = GraduationRequirements.satisfiesUniversityLevelRequirements(modules)
+    val satisfiesCsFoundations: Boolean = GraduationRequirements.satisfiesComputerScienceFoundations(modules)
+    val satisfiesCsBreadthAndDepth: Boolean = GraduationRequirements.satisfiesComputerScienceBreadthAndDepth(modules)
+    val satisfiesIndustrialExperience: Boolean = GraduationRequirements.satisfiesIndustrialExperienceRequirements(modules)
+    val satisfiesItProfessionalism: Boolean = GraduationRequirements.satisfiesItProfessionalism(modules)
+    val satisfiesMathematicsAndSciences: Boolean = GraduationRequirements.satisfiesMathematicsAndSciences(modules)
+    val satisfiesCredits: Boolean = GraduationRequirements.satisfiesCredits(modules)
 }
