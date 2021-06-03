a6362781318b283cb15dde2d038c84131658ada5
aizatazhar <aizat.azharr@gmail.com>
2021-06-03 20:59:22 +0800

    Implement expandable list view in modules page

---
+++ app/src/main/java/com/example/graduation_planner/screens/modules/ModulesExpandableListViewAdapter.kt
@@ -0,0 +1,102 @@
+package com.example.graduation_planner.screens.modules
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.graduation_planner.R
+import com.example.graduation_planner.models.Module
+
+class ModulesExpandableListViewAdapter(
+    var context: Context,
+    private var groupNames: Array<String>,
+    var map: HashMap<String, Array<Module>>
+) : BaseExpandableListAdapter() {
+    override fun getGroupCount(): Int {
+        return map.keys.size
+    }
+
+    override fun getChildrenCount(i: Int): Int {
+        return map[groupNames[i]]?.size ?: 0
+    }
+
+    override fun getGroup(i: Int): Any {
+        return groupNames[i]
+    }
+
+    override fun getChild(listPosition: Int, expandedListPosition: Int): Module? {
+        return map[groupNames[listPosition]]?.get(expandedListPosition)
+    }
+
+    override fun getGroupId(i: Int): Long {
+        return i.toLong()
+    }
+
+    override fun getChildId(p0: Int, p1: Int): Long {
+        return p1.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getGroupView(
+        listPosition: Int,
+        isExpanded: Boolean,
+        view: View?,
+        viewGroup: ViewGroup?
+    ): View? {
+        var convertView = view
+        if (view == null) {
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.module_list_group_layout, null)
+        }
+
+        convertView?.findViewById<TextView>(R.id.title)?.apply {
+            text = groupNames[listPosition]
+        }
+        return convertView
+    }
+
+    override fun getChildView(
+        listPosition: Int,
+        expandedListPosition: Int,
+        isLastChild: Boolean,
+        view: View?,
+        viewGroup: ViewGroup?
+    ): View? {
+        var convertView = view
+        if (view == null) {
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.module_list_child_layout, null)
+        }
+
+        convertView?.findViewById<TextView>(R.id.tvModuleCode)?.apply {
+            text = getChild(listPosition, expandedListPosition)?.moduleCode
+        }
+
+        convertView?.findViewById<TextView>(R.id.tvModuleTitle)?.apply {
+            val title = getChild(listPosition, expandedListPosition)?.title
+            val mcs = getChild(listPosition, expandedListPosition)?.moduleCredit
+            text = "${title} ($mcs} MCs)"
+        }
+
+        convertView?.findViewById<ImageView>(R.id.ivClearButton)?.apply {
+            setOnClickListener {
+                println("Hello world")
+            }
+        }
+
+        return convertView
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        return false
+    }
+
+}
--- app/src/main/java/com/example/graduation_planner/screens/modules/ModulesFragment.kt
+++ app/src/main/java/com/example/graduation_planner/screens/modules/ModulesFragment.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.graduation_planner.R
+import com.example.graduation_planner.models.Module
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class ModulesFragment : Fragment() {
     private val viewModel: ModulesViewModel by activityViewModels()
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var modulesRecyclerAdapter: ModulesRecyclerAdapter
+//    private lateinit var recyclerView: RecyclerView
+//    private lateinit var modulesRecyclerAdapter: ModulesRecyclerAdapter
+
+    private val groupNames = arrayOf("Y1S1", "Y1S2")
+    private val map = hashMapOf("Y1S1" to arrayOf(
+        Module("CS1101S", "Programming Methodology I"),
+        Module("CS1231S", "Discrete Structures")), "Y1S2" to arrayOf(
+        Module("CS2040S", "Data Structures & Algorithms"),
+        Module("CS2030", "Programming Methodology II")
+    ))
+    private lateinit var expandableListView: ExpandableListView
+    private lateinit var adapter: ModulesExpandableListViewAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,14 +34,18 @@ class ModulesFragment : Fragment() {
             : View? {
         val root = inflater.inflate(R.layout.modules_fragment, container, false)
 
-        recyclerView = root.findViewById(R.id.rvModulesAdded)
-        modulesRecyclerAdapter = ModulesRecyclerAdapter(viewModel::deleteModule, viewModel.modules)
-        recyclerView.adapter = modulesRecyclerAdapter
+//        recyclerView = root.findViewById(R.id.rvModulesAdded)
+//        modulesRecyclerAdapter = ModulesRecyclerAdapter(viewModel::deleteModule, viewModel.modules)
+//        recyclerView.adapter = modulesRecyclerAdapter
+//
+//        viewModel.liveModules.observe(viewLifecycleOwner, {
+//            viewModel.liveModules.value?.let { modules -> modulesRecyclerAdapter.submitList(modules) }
+//            viewModel.recalculateGraduationRequirements()
+//        })
 
-        viewModel.liveModules.observe(viewLifecycleOwner, {
-            viewModel.liveModules.value?.let { modules -> modulesRecyclerAdapter.submitList(modules) }
-            viewModel.recalculateGraduationRequirements()
-        })
+        expandableListView = root.findViewById(R.id.modules)
+        adapter = ModulesExpandableListViewAdapter(requireActivity().application, groupNames, map)
+        expandableListView.setAdapter(adapter)
 
         val fabAddButton: ExtendedFloatingActionButton = root.findViewById(R.id.fabAddModule)
         fabAddButton.setOnClickListener {
