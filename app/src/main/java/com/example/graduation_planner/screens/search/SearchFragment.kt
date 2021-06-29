d7ddf93cc21b1c5a76edebac91453ee6e18bcdd9
aizatazhar <aizat.azharr@gmail.com>
2021-06-29 19:06:15 +0800

    Display the selected semester on search screen

--- app/src/main/java/com/example/graduation_planner/screens/search/SearchFragment.kt
+++ app/src/main/java/com/example/graduation_planner/screens/search/SearchFragment.kt
@@ -65,8 +65,11 @@ class SearchFragment : Fragment() {
             }
         })
 
-        binding.semesterChip.setOnClickListener {
-            it.findNavController().navigate(R.id.action_searchFragment_to_semesterFragment)
+        binding.semesterChip.apply {
+            setOnClickListener {
+                it.findNavController().navigate(R.id.action_searchFragment_to_semesterFragment)
+            }
+            text = "Adding to ${viewModel.selectedSemester.toUpperCase()}"
         }
     }
 
